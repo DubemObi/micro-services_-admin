b603ab211d2dc820221c6dd5f7ef1bd44caa35ca
unknown <dubemobiwon@gmail.com>
2023-06-23 02:41:45 -0700

    Admin commit

---
+++ src/product/product.controller.ts
@@ -0,0 +1,71 @@
+import {
+  Controller,
+  Get,
+  Body,
+  Post,
+  Param,
+  Put,
+  Delete,
+  Inject,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Controller('products')
+export class ProductController {
+  constructor(
+    private productService: ProductService,
+    @Inject('PRODUCT_SERVICE') private readonly client: ClientProxy,
+  ) {}
+
+  @Get()
+  async all() {
+    return this.productService.all();
+  }
+
+  @Post()
+  create(@Body('title') title: string, @Body('image') image: string) {
+    const product = this.productService.create({
+      title,
+      image,
+    });
+
+    this.client.emit('product_created', product);
+
+    return product;
+  }
+  @Get(':id')
+  get(@Param('id') id: number) {
+    return this.productService.get(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body('title') title: string,
+    @Body('image') image: string,
+  ) {
+    await this.productService.update(id, {
+      title,
+      image,
+    });
+
+    const product = this.productService.get(id);
+    this.client.emit('product_updated', product);
+
+    return product;
+  }
+  @Delete('id')
+  async delete(@Param('id') id: number) {
+    await this.productService.delete(id);
+    this.client.emit('product_deleted', id);
+  }
+
+  @Post(':id/like')
+  async like(@Param('id') id: number) {
+    const product = await this.productService.get(id);
+    return this.productService.update(id, {
+      likes: product.likes + 1,
+    });
+  }
+}
---
+++ src/product/product.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductController } from './product.controller';
+import { Product } from './product.entity';
+import { ProductService } from './product.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [
+            'amqps://xzyxccrd:Jlhbc9AM3VaAGcC7-DBy1XRjk0kElqpC@hawk.rmq.cloudamqp.com/xzyxccrd',
+          ],
+          queue: 'main_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [ProductController],
+  providers: [ProductService],
+})
+export class ProductModule {}
